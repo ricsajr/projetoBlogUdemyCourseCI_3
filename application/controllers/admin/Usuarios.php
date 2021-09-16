49805c5f84abc2fa855b7cf9b119e58bfe8bc151
Ricardo <ricardo.araujo@neki-it.com.br>
2021-09-16 12:02:25 -0300

    login implementado

--- application/controllers/admin/Usuarios.php
+++ application/controllers/admin/Usuarios.php
@@ -39,4 +39,36 @@ class Usuarios extends CI_Controller {
 
 	}
 
+	public function login(){
+		//carrego a biblioteca para validação de form
+		$this->load->library('form_validation');
+		//listo os campos que desejo validar;(nome do campo, nome de exibição na validação, regras)
+		$this->form_validation->set_rules('txt-user','Usuario','required|min_length[3]');
+		$this->form_validation->set_rules('txt-senha','Senha','required|min_length[3]');
+		if($this->form_validation->run() == FALSE){
+			$this->pag_login();
+		}
+		else{
+			$usuario= $this->input->post('txt-user');
+			$senha= $this->input->post('txt-senha');
+
+			$this->db->where('user', $usuario);//helper db armazena montagem da query
+			$this->db->where('senha', $senha);
+			$userlogado = $this->db->get('usuario')->result();
+			if(count($userlogado)==1){
+				$dadosSessao['userlogado'] = $userlogado[0];
+				$dadosSessao['logado'] = TRUE;
+				$this->session->set_userdata($dadosSessao);
+				redirect(base_url('admin'));
+			}
+			else{
+				$dadosSessao['userlogado'] = NULL;
+				$dadosSessao['logado'] = FALSE;
+				$this->session->set_userdata($dadosSessao);
+				redirect(base_url('admin/login'));
+			}
+
+		}
+	}
+
 }
--- application/views/backend/home.php
+++ application/views/backend/home.php
@@ -15,7 +15,7 @@
 				<div class="panel-body">
 					<div class="row">
 						<div class="col-lg-12">
-							<h2>Bem vindo ao sistema!</h2>
+							<h2>Bem vindo ao sistema! <?= $this->session->userdata('userlogado')->nome; ?></h2>
 						</div>
 
 					</div>
